de3f6313c8c17da7d177972372018c93bd9df61a
Brandon W <walshb1@hawkmail.newpaltz.edu>
2015-11-02 06:19:24 +0000

    translating from C++ to python

--- linv1.py
+++ linv1.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 
-#sparc: big endian
-#linux: little endian
 import sys
 import platform
 
-try:
-  DOS
-except NameError:
-  try:
-  	SPARC
-  except NameError:
-  	try:
-  		LINUX
-  	except NameError:
-  		DOS = 1
+if platform.system() == 'Windows':
+	fdiv = "\\"
+else:
+	fdiv = "/"
+
+#------------------------------
 
 BUF_SIZE = 121
 MACSIZE = 4096
@@ -23,54 +17,64 @@ E_TABLE_SIZE = 5
 R_TABLE_SIZE = 5
 FILE_BUF_SIZE = 12000
 
-class PTYPE(object):
-    address = None
-    symptr = None
+#------------------------------
 
-class ETYPE(object):
-    address = None
-    symptr = None
+class PTYPE:
+	address = ''
+	symptr = ''
 
-class RTYPE(object):
-    address = None
-    module_address = None
+class ETYPE:
+	address = ''
+	symptr = ''
+	
+class RTYPE:
+	address = ''
+	module_address = ''
+	
+#------------------------------
 
-try:
-	SPARC
-except NameError:
-	try:
-		LINUX
-	except NameError:
-		try:
-			DOS
-		except NameError:
-			x=1
-		else:
-			fdiv = '\\'
-	else:
-		fdiv = '/'
-else:
-	fdiv = '/'
-
-print fdiv
-
-author = "linv1 written by Brandon Walsh"
-buf = [None] * BUF_SIZE
-ifilename = None
-ofilename = None
-file_buffer = [None] * FILE_BUF_SIZE
-filesize = 0
+P_tablex = R_tablex = E_tablex = ssize = filesize = textsize = E_tablexstart = R_tablexstart = ofopen = startadd = module_address = 0
+	
+ifilename = ''
 
 def ierror():
-	print "Input file " + str(ifilename) + " is not linkable\n"
+	print("Input file " + str(ifilename) + " is not linkable\n")
 	sys.exit()
 
 def processfile():
-	endptr = file_buffer + filesize
-	fptr = file_buffer
+	firstchar = fptr = endptr = ''
+	i = address = ''
+	
+	#endptr = file_buffer + filesize
+	
+def doifile():
+	pcat = ''
+	
+	if not(ifilename.endswith('.mob')):
+		ifilename + '.mob' 
+	 
+	if platform.system() == 'Windows':
+		in_stream = open(ifilename, 'rb')
+	else:
+		in_stream = open(ifilename, 'r')
+		
+	if not(in_stream):
+		print("ERROR: Cannot open input file" + ifilename)
+    	sys.exit()
 
-	#while(1):
-		#firstchar = 
-
-print sys.byteorder
-print platform.system()
+	if not(ofopen):
+		ofopen = 1
+		ofilename = ifilename
+		ofilename.rsplit( ".", 1 )[ 0 ]	#rid of ./mob
+		ofilename + '.mac' #add .mac
+	
+		if platform.system() == 'Windows':
+			out_stream = open(ofilename, "wb")
+		else:
+			out_stream = open(ofilename, "w")
+			
+		if not(out_stream):
+			print("ERROR: Cannot open output file" + ofilename)
+			sys.exit()
+	
+	
