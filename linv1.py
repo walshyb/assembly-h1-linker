2415a8a56dd264ab598a3f3ddd2475cf1eb752f2
walshyb <walshb1@hawkmail.newpaltz.edu>
2015-11-25 15:56:56 -0500

    outputs 1 byte of 0 if sequence of 2 bytes of 0 is found

--- linv1.py
+++ linv1.py
@@ -17,11 +17,11 @@ class PTYPE:
 class ETYPE:
 	address = 0
 	symptr = 0
-	
+
 class RTYPE:
 	address = 0
 	module_address = 0
-	
+
 #------------------------------
 
 P_table = [PTYPE()] * P_TABLE_SIZE
@@ -29,6 +29,7 @@ E_table = [ETYPE()] * E_TABLE_SIZE
 R_table = [RTYPE()] * R_TABLE_SIZE
 
 P_tablex = R_tablex = E_tablex = filesize = textsize = E_tablexstart = R_tablexstart = ofopen = startadd = module_address = text_buffer_size = 0
+text_buff_index = 0
 gots = False
 saves = ''
 ifilename = ''
@@ -44,12 +45,13 @@ def ierror():
 	sys.exit()
 
 def processfile():
-	global startadd, gots, module_address, P_table, E_table, R_table, P_tablex, E_tablex, R_tablex, text_buffer_size
+	global startadd, gots, module_address, P_table, E_table, R_table, P_tablex, E_tablex, R_tablex, text_buffer_size, text_buff_index
 	i = 0
 	address = 0
 	endptr = file_buffer[len(file_buffer)-1]
+
 	fptr = file_buffer[i]
-	
+
 	while True:
 		firstchar = fptr
 		i += 1
@@ -64,24 +66,52 @@ def processfile():
 			module_address_Save = module_address
 			i_Save = i
 
-			for j in range(i, len(file_buffer)):
+			for i in range(i, len(file_buffer)):
 				file_buffer[i] = hex(ord(file_buffer[i])).lstrip("\\x")
 				file_buffer[i] = int(file_buffer[i], 16)
-				
+
 				if file_buffer[i] == 0:
-					i+=1
+					try:
+						next = file_buffer[i+1]
+						next = hex(ord(next)).lstrip("\\x")
+						next = int(next, 16)
+
+						if next == 0:
+							
+							text_buffer[text_buff_index] = file_buffer[i]
+							text_buff_index += 1
+							text_buffer_size += 1
+							module_address_Save += 1
+							
+							#or
+							#pass
+						else:
+							#or
+							pass
+
+					except IndexError:
+						pass
 				else:
-					text_buffer[module_address] = file_buffer[i]
-					i += 1
-					module_address += 1
+					text_buffer[text_buff_index] = file_buffer[i]
+
+					text_buff_index += 1
+
 					text_buffer_size += 1
-
-			module_address = module_address_Save
+					module_address_Save += 1
+				'''
+				if file_buffer[i] < 10 and file_buffer[i] > 0:
+					text_buffer[text_buff_index] = 0
+					text_buffer_size += 1
+					text_buff_index+=1
+				'''
+			#module_address = (textsize/2) + i_Save #might not be necessary
 			i = i_Save
-			module_address += textsize/2
 
+			#module_address = module_address_Save
+			#module_address += textsize/2 #might not be necessary
+			module_address += textsize/2
 			break
-		
+
 		if firstchar != 'S' and firstchar != 's' and firstchar != 'P' and firstchar != 'E' and firstchar != 'R':
 			ierror()
 		else:
@@ -93,10 +123,10 @@ def processfile():
 				if gots:
 					print("ERROR: More than one starting add")
 					sys.exit()
-				
+
 				gots = True
 				saves = firstchar
-				
+
 				if firstchar == 'S':
 					startadd = address
 				else:
@@ -132,7 +162,7 @@ def processfile():
 					if(firstchar == P_table[j].symptr):
 						print("ERROR: Duplicate PUBLIC symbol " + firstchar)
 						sys.exit()
-				
+
 				P_table[P_tablex].symptr = fptr
 				P_tablex += 1
 			elif firstchar == 'E':
@@ -144,12 +174,12 @@ def processfile():
 			continue
 def doifile():
 	global ifilename, out_stream, ofopen, file_buffer, ofilename
-	
+
 	if not(ifilename.endswith('.mob')):
 		ifilename = ifilename + '.mob'
-	
+
 	file_buffer = []
-	
+
 	in_stream = open(ifilename, 'rb')
 	with in_stream as file:
 		byte = file.read(1)
@@ -158,72 +188,83 @@ def doifile():
 			byte = file.read(1)
 
 	in_stream.close()
-	
+
 	if not(in_stream):
 		print("ERROR: Cannot open input file" + ifilename)
 		sys.exit()
-	
+
 	if not(ofopen):
 		ofopen = 1
 		ofilename = ifilename
 		ofilename = ofilename.rsplit( ".", 1 )[ 0 ]	#rid of ./mob
 		ofilename = ofilename + '.mac' #add .mac
-	
+
 		out_stream = open(ofilename, "wb")
-			
+
 		if not(out_stream):
 			print("ERROR: Cannot open output file" + ofilename)
 			sys.exit()
-	
+
 	processfile()
 
 def main():
 	global ifilename, out_stream, in_stream, E_tablexstart,R_tablexstart,P_tablexstart, text_buffer_size, module_address, R_table
 	j = 0
 	print("Author: Brandon Walsh")
-	
+
 	if len(sys.argv) < 3:
 		print("ERROR: Incorrect number of command line args")
 		sys.exit()
-		
+
 	for argx in range (1, len(sys.argv)):
 		ifilename = sys.argv[argx]
 		doifile()
-		
+
+	#print "\nP_table Address: " + str(P_table[0].address)
+
 	for E_tablexstart in range (E_tablexstart, E_tablex):
 		j = 0
-		
+
 		while j < P_tablex and P_table[j].symptr != E_table[E_tablexstart].symptr:
 			j += 1
-			
-			if j < P_tablex:
-				text_buffer[E_table[E_tablexstart].address] = text_buffer[E_table[E_tablexstart].address] & 0xf000 | (text_buffer[E_table[E_tablexstart].address] + P_table[j].address ) & 0x0fff
-			else:
-				break
-	
+
+		#print P_table[0].address
+		text_buffer[E_table[E_tablexstart].address] = text_buffer[E_table[E_tablexstart].address] & 0xf000 | (text_buffer[E_table[E_tablexstart].address] + P_table[j].address ) & 0x0fff
+		#print text_buffer[E_table[E_tablexstart].address]
+
+	#print text_buffer[:8]
 	'''
 	if E_tablexstart != E_tablex:
 		print("ERROR: Unresolved external symbol " + E_table[E_tablexstart].symptr)
 		sys.exit()
 	'''
+
 	for R_tablexstart in range (R_tablexstart, R_tablex):
 		text_buffer[R_table[R_tablexstart].address] = text_buffer[R_table[R_tablexstart].address] & 0xf000 | (text_buffer[R_table[R_tablexstart].address] + R_table[R_tablexstart].module_address) & 0x0fff
 
+	print text_buffer[:8]
+
 	for i in range (0, P_tablex):
+		#print "P Address: " + str(P_table[i].address)
+		#print "P Symptr: " + P_table[i].symptr
 		out_stream.write('P')
 		out_stream.write(chr(int(str(P_table[i].address), 16)))
 		out_stream.write(chr(00))
 		out_stream.write(P_table[i].symptr)
 		out_stream.write(chr(00))
-		
+
 		#print hex(0x0000 | int(hex(ord("P")), 16))
 
 	for i in range (0, R_tablex):
+		#print "R Address: " + str(R_table[i].address)
+		#print "R Mod Address: " + R_table[i].module_address
 		out_stream.write("R")
 		out_stream.write(chr(int(str(R_table[i].address), 16)))
 		out_stream.write(chr(00))
 
 	for i in range(0, E_tablex):
+		#print "E Address: " + str(E_table[i].address)
+		#print "E Symptr: " + E_table[i].symptr
 		out_stream.write("R")
 		out_stream.write(chr(int(str(E_table[i].address), 16)))
 		out_stream.write(chr(00))
@@ -233,19 +274,23 @@ def main():
 		out_stream.write(startadd)
 
 	out_stream.write("T")
-	
+
 	for k in range(0, text_buffer_size+8):
 	#for k in range(0, module_address * 2): #this might work
+		#print text_buffer[k]
 		if text_buffer[k] >= 255:
 			out_stream.write(chr(text_buffer[k]))
-			out_stream.write(chr(00))
+		elif text_buffer[k] >= 1 and text_buffer[k] < 10:
+			out_stream.write(chr(text_buffer[k]))
+			out_stream.write(chr(0))
 		else:
 			out_stream.write(chr(text_buffer[k]))
-		
+
+		#print text_buffer[k]
 		#print text_buffer[k]
 		#print format(text_buffer[k], '02x')
-		
+
 	out_stream.close()
 	print "Output file: " + ofilename
-	
+
 main()
